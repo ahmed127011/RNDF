d0ff8854822da567f11b0eade60467886f3013b6
ahmed127011 <ahmed127011@gmail.com>
2020-04-24 00:57:01 +0200

    another commit

--- regression/main.py
+++ regression/main.py
@@ -44,7 +44,7 @@ def main():
     
     # sanity check for FG-NET dataset, not used for now   
     # assertion: the total images in the eval set lists should be 1002
-    print(db.shape)
+    print(db["eval"].size)
     total_eval_imgs = sum([len(db['eval'][i]) for i in range(len(db['eval']))])
     print(total_eval_imgs)
     if db['train'][0].name == 'FGNET':
