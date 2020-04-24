323cdf5c161781ef6c31ecf180ce2bcc6165105a
ahmed127011 <ahmed127011@gmail.com>
2020-04-24 17:18:07 +0200

    another commit

--- regression/ndf.py
+++ regression/ndf.py
@@ -333,7 +333,7 @@ class Forest(nn.Module):
                 p = tree.pred(x)
             predictions.append(p.unsqueeze(2))
         print("heererere")
-        print(predictions.shape)
+        print(len(predictions))
         print(self.n_tree)
         prediction = torch.cat(predictions, dim=2)
         prediction = torch.sum(prediction, dim=2) / self.n_tree
