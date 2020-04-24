73109cd950cdaf212653a713dc82c5c165dc0733
ahmed127011 <ahmed127011@gmail.com>
2020-04-24 16:18:15 +0200

    another commit

--- regression/ndf.py
+++ regression/ndf.py
@@ -231,7 +231,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(1)#, stride=1, padding=1)
-        self.fc = nn.Linear(2048 * block.expansion, 1, bias=False)
+        self.fc = nn.Linear(64 , 512, bias=False)
         self.linear_1_bias = nn.Parameter(torch.zeros(self.num_classes - 1).float())
         using_idx = np.random.choice(num_classes, self.n_leaf, replace=False)
         onehot = np.eye(num_classes)
