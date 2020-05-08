de216d790e7efd47a5e2ce83da55def7534158b4
ahmed127011 <ahmed127011@gmail.com>
2020-05-08 05:03:51 +0200

    another commit

--- regression/ndf.py
+++ regression/ndf.py
@@ -245,6 +245,10 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
+    def pred(self, x):
+        p = torch.mm(self(x), self.mean)
+        return p
+
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
